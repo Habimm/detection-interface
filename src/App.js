eb922f9bef9e00b38202ae7f827edef7701e2a00
Dimitri <habimm@pm.me>
2023-05-14 14:39:51 +0200

    new address

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
             redirect: 'follow',
           };
 
-        fetch("https://18.237.216.140:8080/yolo", requestOptions)
+        fetch("https://18.237.217.174:8080/yolo", requestOptions)
           .then(response => response.blob())
           .then(blob => {
             const detectionsBlobUrl = URL.createObjectURL(blob);
